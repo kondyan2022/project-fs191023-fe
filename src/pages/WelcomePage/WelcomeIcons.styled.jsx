58cf109c4bb9033f252116db37925bfc520e2418
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-25 02:59:25 +0300

    try try try

--- src/pages/WelcomePage/WelcomeIcons.styled.jsx
+++ src/pages/WelcomePage/WelcomeIcons.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 // import { ReactComponent as IconCircleDecoration } from '../../images/sprite.svg#icon-Line';
 
-import { ReactComponent as IconRunMen } from '../../images/sprite.svg#icon-run-man';
+// import { ReactComponent as IconRunMen } from '../../images/sprite.svg#icon-run-man';
 // import { ReactComponent as IconPlayVideo } from '../../images/sprite.svg#icon-play';
 
 // export const IconPlayVideoStyled = styled(IconPlayVideo)`
@@ -12,11 +12,11 @@ import { ReactComponent as IconRunMen } from '../../images/sprite.svg#icon-run-m
 //   transform: rotate(90deg);
 // `;
 
-export const IconRunMenStyled = styled(IconRunMen)`
-  fill: var(--color-white);
-  width: 16px;
-  height: 16px;
-`;
+// export const IconRunMenStyled = styled(IconRunMen)`
+//   fill: var(--color-white);
+//   width: 16px;
+//   height: 16px;
+// `;
 
 // export const IconCircleDecorationStyled = styled(IconCircleDecoration)`
 //   stroke-width: 2px;
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -4,11 +4,11 @@ import Container from '../../components/Container/Container';
 import WelcomeInfoCalories from '../../components/WelcomeInfoCalories/WelcomeInfoCalories';
 import WelcomeInfoVideo from '../../components/WelcomeInfoVideo/WelcomeInfoVideo';
 import { Title, WrappTitle, WrapperFlex } from './WelcomePage.styled';
-import {
-  // IconCircleDecorationStyled,
-  // IconPlayVideoStyled,
-  IconRunMenStyled,
-} from './WelcomeIcons.styled';
+// import {
+//   // IconCircleDecorationStyled,
+//   // IconPlayVideoStyled,
+//   IconRunMenStyled,
+// } from './WelcomeIcons.styled';
 import { buttonStyles } from '../../components/Button/Button.styled';
 import { Section } from '../../components/Section/Section.styled';
 
