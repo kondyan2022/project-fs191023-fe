e44168d04461816fdf236c4e52d84f41167279b4
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-25 02:55:11 +0300

    try to fix 4

--- src/pages/WelcomePage/WelcomeIcons.styled.jsx
+++ src/pages/WelcomePage/WelcomeIcons.styled.jsx
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 import { ReactComponent as IconCircleDecoration } from '../../images/sprite.svg#icon-Line';
 
 import { ReactComponent as IconRunMen } from '../../images/sprite.svg#icon-run-man';
-import { ReactComponent as IconPlayVideo } from '../../images/sprite.svg#icon-play';
+// import { ReactComponent as IconPlayVideo } from '../../images/sprite.svg#icon-play';
 
-export const IconPlayVideoStyled = styled(IconPlayVideo)`
-  fill: var(--color-white);
-  width: 20px;
-  height: 20px;
-  transform: rotate(90deg);
-`;
+// export const IconPlayVideoStyled = styled(IconPlayVideo)`
+//   fill: var(--color-white);
+//   width: 20px;
+//   height: 20px;
+//   transform: rotate(90deg);
+// `;
 
 export const IconRunMenStyled = styled(IconRunMen)`
   fill: var(--color-white);
