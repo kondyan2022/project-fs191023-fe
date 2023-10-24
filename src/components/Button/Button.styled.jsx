11e7013aa49d44ff177d09de127e0724658e5481
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-24 08:18:57 +0300

    add swiper package

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -5,13 +5,12 @@ export const buttonStyles = {
     color: 'var(--color-white)',
     borderRadius: '12px',
     height: '42px',
-    whiteSpace: 'normal',
-    fontFamily: 'Roboto', 
-    fontWeight: 500,   
-    letterSpacing: '0em',    
-    fontSize: '16px', 
-    lineHeight: '18px', 
-    padding: '12px 40px', 
+    fontFamily: 'Roboto',
+    fontWeight: 500,
+    letterSpacing: '0em',
+    fontSize: '16px',
+    lineHeight: '18px',
+    padding: '12px 40px',
     overflow: 'hidden',
     whiteSpace: 'nowrap',
     extOverflow: 'ellipsis',
@@ -32,20 +31,23 @@ export const buttonStyles = {
 };
 
 export const StyledButton = styled.button`
-  ${(props) => buttonStyles.common};
-  ${(props) => (props.primary ? buttonStyles.primaryButtonStyles : buttonStyles.secondaryButtonStyles)};
-  
+  ${() => buttonStyles.common};
+  ${(props) =>
+    props.primary
+      ? buttonStyles.primaryButtonStyles
+      : buttonStyles.secondaryButtonStyles};
+
   @media (min-width: 375px) and (max-width: 768px) {
-    width: ${(props) => (props.primary ? '136px' : '130px')}; 
-    font-size: 16px; 
-    line-height: 18px; 
-    padding: '12px 40px'; 
+    width: ${(props) => (props.primary ? '136px' : '130px')};
+    font-size: 16px;
+    line-height: 18px;
+    padding: '12px 40px';
   }
 
   @media (min-width: 768px) {
-    width: '190px'; 
-    font-size: 20px; 
-    line-height: 24px; 
-    padding: '16px 60px'; 
+    width: '190px';
+    font-size: 20px;
+    line-height: 24px;
+    padding: '16px 60px';
   }
 `;
