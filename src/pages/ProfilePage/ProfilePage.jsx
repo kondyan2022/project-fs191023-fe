63b980e22b02d982c282c8869820bd77d19b1e68
ArIonMark1 <andreydatsenko777@gmail.com>
2023-10-25 22:23:08 +0200

    redux

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,19 +1,21 @@
-
 import { Container } from '../../components/Container/Container';
 import UserCard from '../../components/UserCard/UserCard';
 import { Title, WrapperContent } from './ProfilePage.styled';
 import UserForm from '../../components/UserForm/UserForm';
 
+import { useUserLogOutMutation } from '../../redux/features/authEndpoints';
+
 const ProfilePage = () => {
+  const [handleFunction, { data, isF }] = useUserLogOutMutation();
   return (
     <Container>
-      <Title>Profile Settings</Title>      
-    <UserForm />
+      <Title>Profile Settings</Title>
+      <UserForm />
       <WrapperContent>
         <UserCard />
       </WrapperContent>
       {/* <Nottice /> */}
-      {/* <Logout /> */}      
+      {/* <Logout /> */}
     </Container>
   );
 };
--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,9 +1,11 @@
 import Container from '../../components/Container/Container';
+import { TestSignIn } from '../../components/TestSignIn/TestSignIn';
 
 const SignInPage = () => {
   return (
     <Container>
       <div>SignIn Page</div>
+      <TestSignIn />
     </Container>
   );
 };
