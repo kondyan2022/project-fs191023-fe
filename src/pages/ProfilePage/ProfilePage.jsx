a15bff78caa35488a86d894f308683bb9c099ef2
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-10-25 23:31:13 +0300

    add header

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,9 @@
 import Container from '../../components/Container/Container';
 
-import UserForm from '../../components/UserForm/UserForm';
 const ProfilePage = () => {
   return (
     <Container>
       <div>Profile Page</div>
-      <UserForm />
     </Container>
   );
 };
