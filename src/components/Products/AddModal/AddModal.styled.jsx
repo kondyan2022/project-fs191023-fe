6f4b55e0e7fb13f05df18d9929f0d257ea05db98
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-10-25 14:05:24 +0200

    Add validation in modal

--- src/components/Products/AddModal/AddModal.styled.jsx
+++ src/components/Products/AddModal/AddModal.styled.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Field } from 'formik';
 
 export const Backdrop = styled.div`
   width: 100vw;
@@ -29,7 +30,8 @@ export const CloseButton = styled.button`
   padding: 0;
 `;
 
-export const Form = styled.form`
+export const FormBlock = styled.div`
+  position: relative;
   display: flex;
   gap: 16px;
   margin-bottom: 16px;
@@ -49,36 +51,43 @@ export const Form = styled.form`
   }
 `;
 
-export const Grams = styled.div`
-  position: relative;
-  input {
-    height: 40px;
-    width: 155px;
-    padding: 8px 0;
-    padding-left: 14px;
-    line-height: 24px;
-    border-radius: 12px;
-    border: 1px solid var(--color-orange);
-    color: var(--color-white);
-    background-color: inherit;
+export const Input = styled(Field)`
+  height: 40px;
+  width: 155px;
+  padding: 8px 0;
+  padding-left: 14px;
+  line-height: 24px;
+  border-radius: 12px;
+  border: 1px solid var(--color-orange);
+  color: var(--color-white);
+  background-color: inherit;
 
-    &:hover,
-    &:focus {
-      outline: none;
-    }
-  }
-
-  p {
-    position: absolute;
-    top: 10px;
-    right: 14px;
-    color: rgba(239, 237, 232, 0.4);
-    font-size: 12px;
-    line-height: 18px;
+  &:hover,
+  &:focus {
+    outline: none;
   }
 `;
 
+export const GramsText = styled.p`
+  position: absolute;
+  top: 10px;
+  right: 44px;
+  color: rgba(239, 237, 232, 0.4);
+  font-size: 12px;
+  line-height: 18px;
+`;
+
+export const ErrorMessage = styled.p`
+  color: var(--color-rad);
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0.12px;
+`;
+
 export const Calories = styled.p`
+  position: absolute;
+  top: 56px;
+  left: 0;
   color: rgba(239, 237, 232, 0.4);
   font-size: 12px;
   line-height: 18px;
@@ -90,7 +99,10 @@ export const Calories = styled.p`
 `;
 
 export const ButtonsList = styled.ul`
+  position: absolute;
+  z-index: 44;
   left: 0;
+  top: 138px;
   display: flex;
   gap: 16px;
 `;
