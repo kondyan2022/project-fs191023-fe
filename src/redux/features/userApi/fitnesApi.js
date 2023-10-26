fffe80c57d87a8e26a53b10848af0252b1f4e14c
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-10-27 02:46:28 +0300

    fix fix fix

--- src/redux/features/userApi/fitnesApi.js
+++ src/redux/features/userApi/fitnesApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const base_url = 'https://power-plus-service.onrender.com';
+// const base_url = 'https://power-plus-service.onrender.com';
 
 export const userSplitApi = createApi({
   //
   reducerPath: 'fitnesApi',
   baseQuery: fetchBaseQuery({
     // базовий урл нашого бекенду
-    baseUrl: base_url,
+    baseUrl: 'https://power-plus-service.onrender.com',
     prepareHeaders: (headers, { getState }) => {
       // збираємо хедер, додаємо токен якщо токен існує у стейті, інакше повертаємо хедер без токена
       const token = getState().token.token; // тягнемо токен зі стору
