8519de60ae68c3c7bd0300a7f756efc1596280ae
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-10-27 13:12:39 +0300

    added BasicModalWindow

--- src/components/BasicModalWindow/BasicModalWindow.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.jsx
@@ -1,9 +1,44 @@
 import { createPortal } from 'react-dom';
-import { MainWindow } from './BasicModalWindow.styled';
+import {
+  ButtonClose,
+  MainWindow,
+  Modal,
+  SvgClose,
+} from './BasicModalWindow.styled';
+import sprite from '../../images/sprite.svg';
+import { useEffect } from 'react';
 
-const BasicModalWindow = ({ children }) => {
+const BasicModalWindow = ({ children, isToggleModal }) => {
+  useEffect(() => {
+    const closeESC = (e) => {
+      if (e.code === 'Escape') {
+        isToggleModal();
+      }
+    };
+    document.addEventListener('keydown', closeESC);
+
+    return () => {
+      document.removeEventListener('keydown', closeESC);
+    };
+  }, [isToggleModal]);
+
+  const handleClickBackground = (e) => {
+    if (e.currentTarget === e.target) {
+      isToggleModal();
+    }
+  };
+
   const modal = () => {
-    <MainWindow>{children}</MainWindow>;
+    <MainWindow onClick={handleClickBackground}>
+      <Modal>
+        <ButtonClose onClick={() => isToggleModal()}>
+          <SvgClose>
+            <use href={`${sprite}#icon-x`}></use>
+          </SvgClose>
+        </ButtonClose>
+      </Modal>
+      {children}
+    </MainWindow>;
   };
   return createPortal(modal, document.querySelector('#modal-window'));
 };
--- src/components/BasicModalWindow/BasicModalWindow.styled.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.styled.jsx
@@ -11,3 +11,32 @@ export const MainWindow = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const Modal = styled.div`
+  border-radius: 12px;
+  border: 1px solid var(--accent-color-border);
+  background: #10100f;
+  position: relative;
+`;
+
+export const ButtonClose = styled.button`
+  width: 22px;
+  height: 22px;
+  border: none;
+  background: inherit;
+  position: absolute;
+  top: 14px;
+  right: 14px;
+
+  @media screen and (min-width: 768px) {
+    width: 26px;
+    height: 26px;
+  }
+`;
+
+export const SvgClose = styled.svg`
+  width: 20px;
+  height: 20px;
+
+  stroke: var(--color-modal-btn-close);
+`;
