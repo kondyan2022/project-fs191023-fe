96e2f68d5a235d067630908241a55e6831ac8f14
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-10-27 23:06:37 +0200

    added styles on welcome cards

--- src/pages/WelcomePage/WelcomeIcons.styled.jsx
+++ src/pages/WelcomePage/WelcomeIcons.styled.jsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
 export const IconPlayVideoStyled = styled.svg`
+position: absolute;
     fill: var(--color-white);
     width: 12px;
     height: 12px;
+    bottom: 9px;
+    left: 10px;
         @media(min-width: 768px) {
             width: 20px;
             height: 20px;
+            bottom: 9.5px;
+            left: 12px;
     }
 `;
 
@@ -21,6 +26,7 @@ export const IconRunMenStyled = styled.svg`
 `;
 
 export const IconCircleDecorStyled = styled.svg`
+    z-index: -1;
     stroke-width: 2px;
     stroke: var(--color-orange-one);
     fill: transparent;
