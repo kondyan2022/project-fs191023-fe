8ea7f448b0827baf180e2cafa751413007862c11
ArIonMark1 <andreydatsenko777@gmail.com>
2023-10-31 01:13:03 +0100

    handle token

--- src/hooks/controlCurrentUser.js
+++ src/hooks/controlCurrentUser.js
@@ -1,83 +1,30 @@
-import { check } from 'prettier';
-import { useGetCurrentUserQuery } from '../redux/features/authEndpoints';
-import { selectToken } from '../redux/selectors';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { logOut } from '../redux/features/userToken'; // очищення глобального стейту
 import { useUserLogOutMutation } from '../redux/features/authEndpoints';
-import { useEffect, useState } from 'react';
-import React from 'react';
 import { useJwt } from 'react-jwt';
-const token = 'Your JWT';
+import { useEffect } from 'react';
 
-// export const getCurrentUserHook = (queryKey, queryFn, options = {}) => {
-//   const isToken = useSelector(selectToken);
-
-//   const { data, error, ...args } = useGetCurrentUserQuery(
-//     queryKey,
-//     () => {
-//       if (!isToken) {
-//         // перевірити чи є токен
-//         throw new Error('No token available');
-//       }
-//       return queryFn();
-//     },
-//     { ...options, retry: false },
-//   );
-
-//   const isLoading = args.status === 'loading';
-//   const isSuccess = args.status === 'success';
-//   const isError = args.status === 'error';
-//   console.log('Inside custom hook');
-
-//   return { ...args };
-//   //   return {
-//   //     data,
-//   //     error,
-//   //     isLoading,
-//   //     isSuccess,
-//   //     isError,
-//   //   };
-// };
-
-const ExpirationControl = ({ children }) => {
+export const useTokenExpirationCheck = (token, checkInterval = 60000) => {
   const dispatch = useDispatch();
-  const tokenInState = useSelector(selectToken); // токен
   const [userLogOut] = useUserLogOutMutation(); // хук логаута користувача
-  const { decodedToken, isExpired } = useJwt(tokenInState);
-  //
-  if (isExpired) {
-    userLogOut(tokenInState);
-    dispatch(logOut());
-  }
+  const { isExpired } = useJwt(token);
 
-  // const isValidToken = isExpired(tokenInState);
-  console.log('Decoded token: ', decodedToken);
-  console.log('Token is Expired: ', isExpired);
+  const checkTokenExpiration = () => {
+    if (token) {
+      console.log('IS EXPIRED TOKEN', ':', isExpired);
+      if (isExpired) {
+        // Токен протух, викликаємо функцію для виходу
+        userLogOut();
+        dispatch(logOut());
+      }
+    }
+  };
 
-  return children;
+  useEffect(() => {
+    checkTokenExpiration();
+    // Почати періодичну перевірку стану токену
+    const intervalId = setInterval(checkTokenExpiration, checkInterval);
+    // Зупинити перевірку при виході з компонента
+    return () => clearInterval(intervalId);
+  }, [checkInterval, userLogOut, dispatch]);
 };
-
-// import { useState, useEffect } from 'react';
-// import { useLogout } from './useLogout'; // Припустимо, що у вас є окремий хук для виходу
-
-// const useTokenExpirationCheck = (token, checkInterval = 60000) => {
-//   // const logout = useLogout(); // Отримати функцію для виходу користувача
-
-//   useEffect(() => {
-//     let intervalId;
-//     const checkTokenExpiration = () => {
-//       // Перевірити тут стан токену, наприклад, час його закінчення
-//       if (token && token.expiresAt <= Date.now()) {
-//         // Токен протух, викликаємо функцію для виходу
-//         // logout();
-//       }
-//     };
-//     // Почати періодичну перевірку стану токену
-//     intervalId = setInterval(checkTokenExpiration, checkInterval);
-
-//     // Зупинити перевірку при виході з компонента
-//     return () => clearInterval(intervalId);
-//   }, [token, checkInterval, logout]);
-// };
-
-export default ExpirationControl;
