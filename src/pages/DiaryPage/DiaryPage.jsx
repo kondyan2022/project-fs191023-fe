4c0934abca82488b4e40e5f746c3e5435bc5ce78
Karavang <k4r4v4ng@gmail.com>
2023-10-31 12:04:20 +0100

    delete from diary page

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,36 +1,17 @@
-import Container from '../../components/Container/Container';
-import { OnTheDay } from '../../components/DayDoneEaten/OnTheDay/OnTheDay';
-import image from '../../images/side-gym-desktop-tab-2x.jpg';
-import { DayDashboard } from '../../components/DayDashboard/DayDashboard';
-import { PageName, DescktopStyle, HederOfPage } from './DiaryPage.styled';
-import dayStatistics from '../../../resources/dayStatistics.json';
-import oneProductTest from '../../../resources/oneProductTest.json';
-import exercisesTest from '../../../resources/exercisesTest.json';
 import DayInformation from '../../../resources/DayInformation.json';
+import dayStatistics from '../../../resources/dayStatistics.json';
+import exercisesTest from '../../../resources/exercisesTest.json';
+import oneProductTest from '../../../resources/oneProductTest.json';
 import StyledDatepicker from '../../components/Calendar/StyledDatepicker';
-import { useState } from 'react';
-import ExerciseCard from '../../components/Exersize-card/ExerciseCard';
-const diary = {
-  name: 'Jeka',
-  target: 'Strength',
-  bodyPart: 'Legs',
-  equipment: 'Dumbbells',
-  gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0009.gif',
-  time: 1,
-  date: '2023/10/24',
-  id: '64f2458d6f67bc34bae4f7f7',
-  burnedCalories: 300,
-};
+import Container from '../../components/Container/Container';
+import { DayDashboard } from '../../components/DayDashboard/DayDashboard';
+import { OnTheDay } from '../../components/DayDoneEaten/OnTheDay/OnTheDay';
+import { DescktopStyle, HederOfPage, PageName } from './DiaryPage.styled';
+
 const DiaryPage = () => {
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsAddModalOpen(true);
-  };
-
   return (
     <>
-      <Container onClick={openModal}>
+      <Container>
         <div>
           <HederOfPage>
             <PageName>Diary</PageName>
@@ -50,9 +31,6 @@ const DiaryPage = () => {
           </DescktopStyle>
         </div>
       </Container>
-      {isAddModalOpen && (
-        <ExerciseCard {...diary} setIsAddModalOpen={setIsAddModalOpen} />
-      )}
     </>
   );
 };
