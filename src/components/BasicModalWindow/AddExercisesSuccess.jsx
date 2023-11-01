4800c213f40e8ee491459855fefebce146807904
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-11-02 01:33:16 +0200

    fix

--- src/components/BasicModalWindow/AddExercisesSuccess.jsx
+++ src/components/BasicModalWindow/AddExercisesSuccess.jsx
@@ -17,113 +17,112 @@ import {
 } from './AddProductSuccess.styled';
 // <<<<<<< exercise-card-fix
 
-// const AddExercisesSuccess = ({
-//   burnedCalories,
-//   time,
-//   closeModal,
-//   isAddModalOpen,
-// }) => {
-//   return (
-//     <>
-//       {isAddModalOpen && (
-//         <ModalWindow>
-//           <ModalWindowWrap>
-//             <ModalWindowWrapImg>
-//               <ModalWindowImg src={like} alt="like" />
-//               <ModalWindowTitle>Well done</ModalWindowTitle>
-//               <ModalWindowText>
-//                 Your time:{' '}
-//                 <ModalWindowSpan>{time ? time : 0} minutes</ModalWindowSpan>
-//               </ModalWindowText>
-//               <ModalWindowText>
-//                 Burned calories::{' '}
-//                 <ModalWindowSpan>
-//                   {burnedCalories ? burnedCalories : 0}
-//                 </ModalWindowSpan>
-//               </ModalWindowText>
-//             </ModalWindowWrapImg>
-//             <Link to="/exercises" onClick={closeModal}>
-//               <ModalWindowBtn>Next exercise</ModalWindowBtn>
-//             </Link>
-//             <Link to="/diary" onClick={closeModal}>
-//               <ModalWindowText>
-//                 To the diary
-//                 <ArrowRightIcon>
-//                   <use href={`${sprite}#icon-arrow-right`}> </use>
-//                 </ArrowRightIcon>
-//               </ModalWindowText>
-//             </Link>
-//           </ModalWindowWrap>
-//         </ModalWindow>
-//       )}
-//     </>
-// =======
+const AddExercisesSuccess = ({
+  burnedCalories,
+  time,
+  closeModal,
+  isAddModalOpen,
+}) => {
+  return (
+    <>
+      {isAddModalOpen && (
+        <ModalWindow>
+          <ModalWindowWrap>
+            <ModalWindowWrapImg>
+              <ModalWindowImg src={like} alt="like" />
+              <ModalWindowTitle>Well done</ModalWindowTitle>
+              <ModalWindowText>
+                Your time:{' '}
+                <ModalWindowSpan>{time ? time : 0} minutes</ModalWindowSpan>
+              </ModalWindowText>
+              <ModalWindowText>
+                Burned calories::{' '}
+                <ModalWindowSpan>
+                  {burnedCalories ? burnedCalories : 0}
+                </ModalWindowSpan>
+              </ModalWindowText>
+            </ModalWindowWrapImg>
+            <Link to="/exercises" onClick={closeModal}>
+              <ModalWindowBtn>Next exercise</ModalWindowBtn>
+            </Link>
+            <Link to="/diary" onClick={closeModal}>
+              <ModalWindowText>
+                To the diary
+                <ArrowRightIcon>
+                  <use href={`${sprite}#icon-arrow-right`}> </use>
+                </ArrowRightIcon>
+              </ModalWindowText>
+            </Link>
+          </ModalWindowWrap>
+        </ModalWindow>
+      )}
+    </>
+    // =======
 
-import { createPortal } from 'react-dom';
-import {
-  Backdrop,
-  CloseButton,
-} from '../../components/Products/AddModal/AddModal.styled.jsx';
+    // import { createPortal } from 'react-dom';
+    // import {
+    //   Backdrop,
+    //   CloseButton,
+    // } from '../../components/Products/AddModal/AddModal.styled.jsx';
 
-const AddExercisesSuccess = ({ burnedCalories, time, closeModal }) => {
-  useEffect(() => {
-    const closeESC = (e) => {
-      if (e.code === 'Escape') {
-        closeModal();
-      }
-    };
-    document.addEventListener('keydown', closeESC);
+    // const AddExercisesSuccess = ({ burnedCalories, time, closeModal }) => {
+    //   useEffect(() => {
+    //     const closeESC = (e) => {
+    //       if (e.code === 'Escape') {
+    //         closeModal();
+    //       }
+    //     };
+    //     document.addEventListener('keydown', closeESC);
 
-    return () => {
-      document.removeEventListener('keydown', closeESC);
-    };
-  }, [closeModal]);
+    //     return () => {
+    //       document.removeEventListener('keydown', closeESC);
+    //     };
+    //   }, [closeModal]);
 
-  const handleClickBackground = (e) => {
-    if (e.currentTarget === e.target) {
-      closeModal();
-    }
-  };
+    //   const handleClickBackground = (e) => {
+    //     if (e.currentTarget === e.target) {
+    //       closeModal();
+    //     }
+    //   };
 
-  return createPortal(
-    <Backdrop onClick={handleClickBackground}>
-      <ModalWindow>
-        <CloseButton onClick={closeModal}>
-          <svg>
-            <use href={`${sprite}#icon-x`}></use>
-          </svg>
-        </CloseButton>
-        <ModalWindowWrap>
-          <ModalWindowWrapImg>
-            <ModalWindowImg src={like} alt="like" />
-            <ModalWindowTitle>Well done</ModalWindowTitle>
-            <ModalWindowText>
-              Your time:{' '}
-              <ModalWindowSpan>{time ? time : 0} minutes</ModalWindowSpan>
-            </ModalWindowText>
-            <ModalWindowText>
-              Burned calories::{' '}
-              <ModalWindowSpan>
-                {burnedCalories ? burnedCalories : 0}
-              </ModalWindowSpan>
-            </ModalWindowText>
-          </ModalWindowWrapImg>
-          <Link to="/exercises" onClick={closeModal}>
-            <ModalWindowBtn>Next exercise</ModalWindowBtn>
-          </Link>
-          <Link to="/diary" onClick={closeModal}>
-            <ModalWindowText>
-              To the diary
-              <ArrowRightIcon>
-                <use href={`${sprite}#icon-arrow-right`}> </use>
-              </ArrowRightIcon>
-            </ModalWindowText>
-          </Link>
-        </ModalWindowWrap>
-      </ModalWindow>
-    </Backdrop>,
-    document.querySelector('#modal-root'),
-
+    //   return createPortal(
+    //     <Backdrop onClick={handleClickBackground}>
+    //       <ModalWindow>
+    //         <CloseButton onClick={closeModal}>
+    //           <svg>
+    //             <use href={`${sprite}#icon-x`}></use>
+    //           </svg>
+    //         </CloseButton>
+    //         <ModalWindowWrap>
+    //           <ModalWindowWrapImg>
+    //             <ModalWindowImg src={like} alt="like" />
+    //             <ModalWindowTitle>Well done</ModalWindowTitle>
+    //             <ModalWindowText>
+    //               Your time:{' '}
+    //               <ModalWindowSpan>{time ? time : 0} minutes</ModalWindowSpan>
+    //             </ModalWindowText>
+    //             <ModalWindowText>
+    //               Burned calories::{' '}
+    //               <ModalWindowSpan>
+    //                 {burnedCalories ? burnedCalories : 0}
+    //               </ModalWindowSpan>
+    //             </ModalWindowText>
+    //           </ModalWindowWrapImg>
+    //           <Link to="/exercises" onClick={closeModal}>
+    //             <ModalWindowBtn>Next exercise</ModalWindowBtn>
+    //           </Link>
+    //           <Link to="/diary" onClick={closeModal}>
+    //             <ModalWindowText>
+    //               To the diary
+    //               <ArrowRightIcon>
+    //                 <use href={`${sprite}#icon-arrow-right`}> </use>
+    //               </ArrowRightIcon>
+    //             </ModalWindowText>
+    //           </Link>
+    //         </ModalWindowWrap>
+    //       </ModalWindow>
+    //     </Backdrop>,
+    //     document.querySelector('#modal-root'),
   );
 };
 
