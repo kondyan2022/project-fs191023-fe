c57453ebbbac216847fa5acf054dd2dec6af7737
ArIonMark1 <andreydatsenko777@gmail.com>
2023-11-01 16:14:52 +0100

    new hook with control token

---
+++ src/hooks/handleCurrentUser.js
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  useGetCurrentUserQuery,
+  useUserLogOutMutation,
+} from '../redux/features/authEndpoints';
+import { logOut } from '../redux/features/userToken';
+import { selectToken } from '../redux/selectors';
+
+export const handleCurrentUser = () => {
+  const dispatch = useDispatch();
+  const [routeLogout] = useUserLogOutMutation();
+  const isTokenExist = useSelector(selectToken);
+  //
+  const { data, error, ...rest } = useGetCurrentUserQuery({
+    skip: !isTokenExist,
+    refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true, // time ? 600 - 1000
+  });
+
+  if (!isTokenExist) {
+    return { data: null };
+  }
+  if (error) {
+    routeLogout(isTokenExist);
+    dispatch(logOut());
+  }
+  // ----------------------------------------------------------------
+  const isLoading = rest.status === 'loading';
+  const isSuccess = rest.status === 'success';
+  const isFetching = rest.status === 'fetching';
+  const isError = rest.status === 'error';
+
+  return { data, error, isSuccess, isLoading, isError, isFetching };
+};
