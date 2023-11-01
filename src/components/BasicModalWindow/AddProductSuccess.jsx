2c1e9d2db11ee15dce04a34c30784f02889de486
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-11-01 02:57:37 +0100

    Add product to diary

--- src/components/BasicModalWindow/AddProductSuccess.jsx
+++ src/components/BasicModalWindow/AddProductSuccess.jsx
@@ -14,31 +14,61 @@ import {
   ModalWindowWrap,
   ModalWindowWrapImg,
 } from './AddProductSuccess.styled';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { Backdrop, CloseButton } from '../Products/AddModal/AddModal.styled';
 
 const AddProductSuccess = ({ excessCalories, closeModal }) => {
-  return (
-    <ModalWindow>
-      <ModalWindowWrap>
-        <ModalWindowWrapImg>
-          <ModalWindowImg src={avocado} alt="avocado" />
-          <ModalWindowTitle>Well done</ModalWindowTitle>
-          <ModalWindowText>
-            Calories: <ModalWindowSpan>{excessCalories}</ModalWindowSpan>
-          </ModalWindowText>
-        </ModalWindowWrapImg>
-        <Link to="/products" onClick={closeModal}>
-          <ModalWindowBtn>Next product</ModalWindowBtn>
-        </Link>
-        <Link to="/diary" onClick={closeModal}>
-          <ModalWindowText>
-            To the diary
-            <ArrowRightIcon>
-              <use href={`${sprite}#icon-arrow-right`}> </use>
-            </ArrowRightIcon>
-          </ModalWindowText>
-        </Link>
-      </ModalWindowWrap>
-    </ModalWindow>
+    useEffect(() => {
+      const closeESC = (e) => {
+        if (e.code === 'Escape') {
+          closeModal();
+        }
+      };
+      document.addEventListener('keydown', closeESC);
+
+      return () => {
+        document.removeEventListener('keydown', closeESC);
+      };
+    }, [closeModal]);
+  
+    const handleClickBackground = (e) => {
+      if (e.currentTarget === e.target) {
+        closeModal();
+      }
+  };
+  
+  return createPortal(
+    <Backdrop onClick={handleClickBackground}>
+      <ModalWindow>
+        <CloseButton onClick={closeModal}>
+          <svg>
+            <use href={`${sprite}#icon-x`}></use>
+          </svg>
+        </CloseButton>
+        <ModalWindowWrap>
+          <ModalWindowWrapImg>
+            <ModalWindowImg src={avocado} alt="avocado" />
+            <ModalWindowTitle>Well done</ModalWindowTitle>
+            <ModalWindowText>
+              Calories: <ModalWindowSpan>{excessCalories}</ModalWindowSpan>
+            </ModalWindowText>
+          </ModalWindowWrapImg>
+          <Link to="/products" onClick={closeModal}>
+            <ModalWindowBtn>Next product</ModalWindowBtn>
+          </Link>
+          <Link to="/" onClick={closeModal}>
+            <ModalWindowText>
+              To the diary
+              <ArrowRightIcon>
+                <use href={`${sprite}#icon-arrow-right`}> </use>
+              </ArrowRightIcon>
+            </ModalWindowText>
+          </Link>
+        </ModalWindowWrap>
+      </ModalWindow>
+    </Backdrop>,
+    document.querySelector('#modal-root'),
   );
 };
 
