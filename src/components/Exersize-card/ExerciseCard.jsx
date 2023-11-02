643e0fc90e4acb9a7b111400a415e27bd8cfe626
Karavang <k4r4v4ng@gmail.com>
2023-11-02 13:40:12 +0100

    backdrop

--- src/components/Exersize-card/ExerciseCard.jsx
+++ src/components/Exersize-card/ExerciseCard.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import iconSvg from '../../images/sprite.svg';
 import CircleCountDown from '../CircleCountDown/CircleCountDown';
-import { StyledAddButton } from '../Products/AddModal/AddModal.styled';
+import {
+  Backdrop,
+  StyledAddButton,
+} from '../Products/AddModal/AddModal.styled';
 import {
   AddButtonPos,
   BurnedPshka,
@@ -89,72 +92,74 @@ const ExerciseCard = ({
     //       calories={calories}
     //     />
     // >>>>>>> main
-    <CardBack>
-      <ListFlex>
-        <li>
-          <ListImgTimer>
-            <li>
-              <ImagePos>
-                <CardImg src={gifUrl} />
-              </ImagePos>
-            </li>
-            <li>
-              <Pshka>Time</Pshka>
-              <CircleCountDown
-                timeInSecond={timeS}
-                size={125}
-                strokeColor={'#E6533C'}
-                strokeBgColor={'#4d4d4d'}
-                strokeWidth={4}
-                tipWidth={7}
-                isRunning={isRunning}
-                setIsRunning={setIsRunning}
-              >
-                {(e) => {
-                  setCalories(Math.round(burnedCalories * (1 - -e)));
-                }}
-              </CircleCountDown>
-              <BurnedPshka>
-                Burned calories:
-                <BurnedPshkaCost>{calories}</BurnedPshkaCost>
-              </BurnedPshka>
-            </li>
-          </ListImgTimer>
-        </li>
-        <li>
-          <CardListInfo>
-            <li>
-              <Pshka>Name</Pshka>
-              <h5>{name}</h5>
-            </li>
-            <li>
-              <Pshka>Target</Pshka>
-              <h5>{target}</h5>
-            </li>
-            <li>
-              <Pshka>Body Part</Pshka>
-              <h5>{bodyPart}</h5>
-            </li>
-            <li>
-              <Pshka>Equipment</Pshka>
-              <h5>{equipment}</h5>
-            </li>
-          </CardListInfo>
-        </li>
-      </ListFlex>
-      <AddButtonPos>
-        {isRunning ? (
-          <StyledDisableButton>Add to diary</StyledDisableButton>
-        ) : (
-          <StyledAddButton onClick={handleAddToDiary}>
-            Add to diary
-          </StyledAddButton>
-        )}
-      </AddButtonPos>
-      <CloseButton onClick={closeModal}>
-        <use href={`${iconSvg}#icon-close`} />
-      </CloseButton>
-    </CardBack>
+    <Backdrop>
+      <CardBack>
+        <ListFlex>
+          <li>
+            <ListImgTimer>
+              <li>
+                <ImagePos>
+                  <CardImg src={gifUrl} />
+                </ImagePos>
+              </li>
+              <li>
+                <Pshka>Time</Pshka>
+                <CircleCountDown
+                  timeInSecond={timeS}
+                  size={125}
+                  strokeColor={'#E6533C'}
+                  strokeBgColor={'#4d4d4d'}
+                  strokeWidth={4}
+                  tipWidth={7}
+                  isRunning={isRunning}
+                  setIsRunning={setIsRunning}
+                >
+                  {(e) => {
+                    setCalories(Math.round(burnedCalories * (1 - -e)));
+                  }}
+                </CircleCountDown>
+                <BurnedPshka>
+                  Burned calories:
+                  <BurnedPshkaCost>{calories}</BurnedPshkaCost>
+                </BurnedPshka>
+              </li>
+            </ListImgTimer>
+          </li>
+          <li>
+            <CardListInfo>
+              <li>
+                <Pshka>Name</Pshka>
+                <h5>{name}</h5>
+              </li>
+              <li>
+                <Pshka>Target</Pshka>
+                <h5>{target}</h5>
+              </li>
+              <li>
+                <Pshka>Body Part</Pshka>
+                <h5>{bodyPart}</h5>
+              </li>
+              <li>
+                <Pshka>Equipment</Pshka>
+                <h5>{equipment}</h5>
+              </li>
+            </CardListInfo>
+          </li>
+        </ListFlex>
+        <AddButtonPos>
+          {isRunning ? (
+            <StyledDisableButton>Add to diary</StyledDisableButton>
+          ) : (
+            <StyledAddButton onClick={handleAddToDiary}>
+              Add to diary
+            </StyledAddButton>
+          )}
+        </AddButtonPos>
+        <CloseButton onClick={closeModal}>
+          <use href={`${iconSvg}#icon-close`} />
+        </CloseButton>
+      </CardBack>
+    </Backdrop>
   );
 };
 export default ExerciseCard;
