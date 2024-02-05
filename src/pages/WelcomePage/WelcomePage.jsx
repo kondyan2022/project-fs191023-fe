30650bf3bd0030bd1245b6a44bb8b96d186d080e
kondyan2022 <kondyan.asusnexus@gmail.com>
2024-02-05 16:16:58 +0200

    Add welcome message

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,11 +2,17 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Container from '../../components/Container/Container';
 import CardInfoVideo from '../../components/CardInfoVideo/CardInfoVideo';
-import { WrappTitle, Wrapper, WrapperFlex, IconCircleDecorStyled } from './WelcomePage.styled';
+import {
+  WrappTitle,
+  Wrapper,
+  WrapperFlex,
+  IconCircleDecorStyled,
+} from './WelcomePage.styled';
 import { buttonStyles } from '../../components/Button/Button.styled';
 import Section from '../../components/Section/Section';
-import iconSvg from '../../images/sprite.svg'
+import iconSvg from '../../images/sprite.svg';
 import CardInfoCalories from '../../components/CardInfoCalories/CardInfoCalories';
+import { red } from '@mui/material/colors';
 
 const WelcomePage = () => {
   const navigate = useNavigate();
@@ -34,8 +40,13 @@ const WelcomePage = () => {
             </Button>
             <Button onClick={handleButtonSignIn} style={buttonStyles}>
               Sign In
-            </Button>            
+            </Button>
           </WrapperFlex>
+          <div style={{ marginTop: '30px', color: 'red', maxWidth: '300px' }}>
+            WARNING! The backend part of the project uses a free service, so
+            there may be a wake-up delay of about 1 minute. Sorry for the
+            inconvenience.
+          </div>
         </Wrapper>
         <CardInfoVideo />
         <CardInfoCalories />
